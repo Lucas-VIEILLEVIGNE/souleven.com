ebb6ea8370db6a62921c441c6b85b438ef01d2fa
Lucas Vieillevigne <lvieillevigne@normandiewebschool.fr>
2022-06-29 00:03:33 +0200

    Dark and Light mode

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,6 +1,7 @@
 function toggleMenu () 
 {
     const mainContent =document.querySelector('.main-content');
+    const Contact =document.querySelector('.box');
     const btn =document.querySelector('.bubbly-button');
     const navbar =document.querySelector('.navbar');
     const burger =document.querySelector('.burger');
@@ -9,14 +10,38 @@ function toggleMenu ()
         navbar.classList.toggle('show-nav');
         if(navbar.classList.contains('show-nav'))
         {
-            mainContent.style.display='none';
-            btn.style.display='none';
+            mainContent.style.display='none'
+            btn.style.display='none'
+            Contact.style.display='none'
         }
         else
         {
             mainContent.style.display='flex'
             btn.style.display='inline-block'
+            Contact.style.display='flex'
         }
     })
 }
 toggleMenu();
+
+
+const DarkBtn =document.querySelector('.dark-btn')  
+
+let toggleTheme =0;
+
+DarkBtn.addEventListener('click', () => {
+
+if(toggleTheme === 0) {
+document.documentElement.style.setProperty('--main-color','#6D0C9D');
+document.documentElement.style.setProperty('--main-bg-color','#C9CDFF');
+document.documentElement.style.setProperty('--background-img','url(/Sources/low-poly-house-light.png)');
+toggleTheme++;
+
+}else{ 
+    document.documentElement.style.setProperty('--main-color', 'white');
+    document.documentElement.style.setProperty('--main-bg-color', '#2E2760');
+    document.documentElement.style.setProperty('--background-img','url(/Sources/low-poly-house.png)');
+    toggleTheme--;
+}
+
+})
